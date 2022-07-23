74caae5a9808009582fe74373b92927369888168
Seungjin <infinissible@gmail.com>
2022-07-23 14:54:53 -0700

    Employee constructor and test completed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,34 @@
-const Employee = require('./Employee');
+const Employee = require('../lib/Employee');
+
+test('create an employee', () => {
+  const employee = new Employee('Sam', 1, 'sam@gmail.com');
+
+  expect(employee.name).toBe('Sam');
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.role).toBe('Employee');
+});
+
+test('test getName() function', () => {
+  const employee = new Employee('Sam', 1, 'sam@gmail.com');
+
+  expect(employee.getName()).toBe('Sam');
+});
+
+test('test getId() function', () => {
+  const employee = new Employee('Sam', 1, 'sam@gmail.com');
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('test getEmail() function', () => {
+  const employee = new Employee('Sam', 1, 'sam@gmail.com');
+
+  expect(employee.getMail()).toEqual(expect.any(String));
+});
+
+test('test getRole() function', () => {
+  const employee = new Employee('Sam', 1, 'sam@gmail.com');
+
+  expect(employee.getRole()).toBe('Employee');
+});
